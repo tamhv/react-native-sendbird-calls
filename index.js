db80c7fa2cd528fc419c459fe9b5049218c559da
Tam Huynh <t.huynh@gooesoft.com>
2022-03-07 10:04:08 +0700

    deploy

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // main index.js
 
 import { NativeModules } from 'react-native';
-import CSendBirdCalls from './CSendBirdCalls'
 const { RNSendBirdCalls } = NativeModules;
 import SendBirdCalls from './SendBirdCalls'
 
 export {
   RNSendBirdCalls,
-  CSendBirdCalls,
   SendBirdCalls
 }
